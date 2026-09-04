e6737ea0789bac29948f04bb91c7ccf71973dc15
sohrabeth9-lab <sohrabeth9@gmail.com>
2026-09-04 15:49:36 +0330

    Update binance_setup_alert_bot.py

--- binance_setup_alert_bot.py
+++ binance_setup_alert_bot.py
@@ -73,7 +73,7 @@ TIMEFRAMES = (
 )
 
 QUOTE_ASSET = os.environ.get("QUOTE_ASSET", "USDT")
-TOP_N = int(os.environ.get("TOP_N", "200"))   # ۲۰۰ نماد برتر بر اساس حجم معاملات (ترکیبی بایننس+مکسی)
+TOP_N = int(os.environ.get("TOP_N", "100"))   # ۲۰۰ نماد برتر بر اساس حجم معاملات (ترکیبی بایننس+مکسی)
 
 # فقط قراردادهای Perpetual مکسی (نه Delivery) در نظر گرفته میشن
 FUTURES_ONLY_PERPETUAL = os.environ.get("FUTURES_ONLY_PERPETUAL", "1") == "1"
